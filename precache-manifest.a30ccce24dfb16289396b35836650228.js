9ad1572da8b2af8e5d937fea890ba57e45097863
Chandra Prakash <pr.chandraprakash@gmail.com>
2020-10-20 15:24:01 +0530

    Updates

---
+++ precache-manifest.a30ccce24dfb16289396b35836650228.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b59401b721e7b9f69fb24804390fa424",
+    "url": "/Meme-generator/index.html"
+  },
+  {
+    "revision": "4831f0b1bfc17b3266e8",
+    "url": "/Meme-generator/static/js/2.02143386.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Meme-generator/static/js/2.02143386.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f21d545def72c6ec8642",
+    "url": "/Meme-generator/static/js/main.5b179536.chunk.js"
+  },
+  {
+    "revision": "c6b487c27cadf437c314",
+    "url": "/Meme-generator/static/js/runtime-main.b97211e2.js"
+  }
+]);
